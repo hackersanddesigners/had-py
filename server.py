6683d7d89713ea200097ef05a42d83668da6d5f5
afincato <andre.not.me@gmail.com>
2017-03-29 16:35:20 +0200

    re-added server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+PORT = 8000
+import http.server
+httpd = http.server.HTTPServer( ("", PORT), http.server.CGIHTTPRequestHandler)
+try:
+		print ("Server Started at port:", PORT)
+		httpd.serve_forever()
+except KeyboardInterrupt:
+		print('\nShutting down server')
+		httpd.socket.close()
