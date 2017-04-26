109d48f1359da812295c935e6985c2d4486b30c6
afincato <andre.not.me@gmail.com>
2017-04-26 17:00:14 +0200

    add article page (def, template, route), clean up urls for nav, etc.

--- had.py
+++ had.py
@@ -20,7 +20,8 @@ class had(object):
     self.url_map = Map([
       Rule('/', endpoint='home'),
       Rule('/<page_title>', endpoint='section'),
-      Rule('/events/<page_title>', endpoint='event')
+      Rule('/Events/<page_title>', endpoint='event'),
+      Rule('/<section_title>/<page_title>', endpoint='article')
     ])
 
   # ===========
@@ -30,10 +31,9 @@ class had(object):
     folder_url = "mediawiki/"
     api_call =  "api.php?"
 
-    nav_options = {'action': 'ask', 'query': '[[Concept:+]]', 'format': 'json', 'formatversion': '2'}
+    nav_options = {'action': 'ask', 'query': '[[Concept:+]]', 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_nav = requests.get(base_url + folder_url + api_call , params=nav_options)
     wkdata_nav = response_nav.json()
-    print(response_nav.url)
 
     return wkdata_nav
 
@@ -45,7 +45,7 @@ class had(object):
     api_call =  "api.php?"
 
     # fetch intro
-    intro_options = {'action': 'parse', 'pageid': '29', 'format': 'json', 'formatversion': '2'}
+    intro_options = {'action': 'parse', 'pageid': '29', 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     intro_response = requests.get(base_url + folder_url + api_call , params=intro_options)
     wkdata_intro = intro_response.json()
 
@@ -74,7 +74,7 @@ class had(object):
     today = today.strftime('%Y/%m/%d')
 
     # attempt to get all event pages recursively (failing atm)
-    options_allevents = {'action': 'query', 'generator': 'categorymembers', 'gcmtitle': 'Category:Event', 'format': 'json', 'formatversion': '2'}
+    options_allevents = {'action': 'query', 'generator': 'categorymembers', 'gcmtitle': 'Category:Event', 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_allevents = requests.get(base_url + folder_url + api_call, params=options_allevents)
     wkdata_allevents = response_allevents.json()
 
@@ -85,19 +85,18 @@ class had(object):
       ev_list = list(dict.items())
       ev_list = ev_list[0][1]
       ev_pageid_list.append(ev_list)
-    #print(ev_pageid_list)
 
     # ===============
     # upcoming events
     date_upevents = "[[OnDate::>" + today + "]]"
-    upevents_options = {'action': 'ask', 'query': category_events + date_upevents + filters_events, 'format': 'json', 'formatversion': '2'}
+    upevents_options = {'action': 'ask', 'query': category_events + date_upevents + filters_events, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_upevents = requests.get(base_url + folder_url + api_call , params=upevents_options)
     wkdata_upevents = response_upevents.json()
 
     for item in wkdata_upevents['query']['results'].items(): 
       upevents_title = item[1]['printouts']['NameOfEvent'][0]['fulltext']
       
-      upevents_introtext_options = {'action': 'parse', 'page': upevents_title, 'format': 'json', 'formatversion': '2'}
+      upevents_introtext_options = {'action': 'parse', 'page': upevents_title, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
       response_introtext_upevents = requests.get(base_url + folder_url + api_call , params=upevents_introtext_options)
       wkdata_introtext_upevents = response_introtext_upevents.json()
 
@@ -111,7 +110,7 @@ class had(object):
 
     # past events
     date_pastevents = "[[OnDate::<" + today + "]]"
-    options_pastevents = {'action': 'ask', 'query': category_events + date_pastevents + filters_events, 'format': 'json', 'formatversion': '2'}
+    options_pastevents = {'action': 'ask', 'query': category_events + date_pastevents + filters_events, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_pastevents = requests.get(base_url + folder_url + api_call , params=options_pastevents)
     wkdata_pastevents = response_pastevents.json()
 
@@ -146,13 +145,20 @@ class had(object):
     api_call =  "api.php?"
 
     # fetch page-content
-    page_head_options = {'action': 'parse', 'page': 'Concept:' + page_title, 'format': 'json', 'formatversion': '2'}
+    page_head_options = {'action': 'parse', 'page': 'Concept:' + page_title, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_head = requests.get(base_url + folder_url + api_call, params=page_head_options)
     wkdata_head = response_head.json()
     wk_title = wkdata_head['parse']['title']
     
+    # fetch page-metadata
+    category_events = "[[Category:Event]]"
+    page_meta_filter = "|?PeopleOrganisations"
+    page_meta_options = {'action': 'browsebysubject', 'subject': page_title, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
+    response_meta = requests.get(base_url + folder_url + api_call, params=page_meta_options)
+    wkdata_meta = response_meta.json()
+
     # fetch page-content
-    page_content_options = {'action': 'ask', 'query': '[[Concept:' + page_title + ']]', 'format': 'json', 'formatversion': '2'}
+    page_content_options = {'action': 'ask', 'query': '[[Concept:' + page_title + ']]', 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_content = requests.get(base_url + folder_url + api_call, params=page_content_options)
     wkdata_content = response_content.json()
 
@@ -169,35 +175,46 @@ class had(object):
     api_call =  "api.php?"
 
     # fetch page-content
-    page_options = {'action': 'parse', 'page': page_title, 'format': 'json', 'formatversion': '2'}
+    page_options = {'action': 'parse', 'page': page_title, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
     response_content = requests.get(base_url + folder_url + api_call, params=page_options)
     wkdata = response_content.json()
     print(response_content.url)
-    print(wkdata)
-
+    
     wktitle = wkdata['parse']['title']
     wkbodytext = wkdata['parse']['text']
 
     wkmeta = wkdata['parse']['links']
-    wkdate = wkmeta[1]['title']
+    #wkdate = wkmeta[1]['title']
+
+    # fetch page-metadata
+    # category_events = "[[Category:Event]]"
+    # page_meta_filter = "|?PeopleOrganisations"
+    # page_meta_options = {'action': 'browsebysubject', 'subject': page_title, 'format': 'json', 'formatversion': '2'}
+    # response_meta = requests.get(base_url + folder_url + api_call, params=page_meta_options)
+    # wkdata_meta = response_meta.json()
+
+    # # extract values from `PeopleOrganisations`
+    # wk_peopleorgs = []
+    # for item in wkdata_meta['query']['data'][2]['dataitem']:
+    #   str = item['item']
+    #   # strip out weird hash at the end (see why https://www.semantic-mediawiki.org/wiki/Ask_API#BrowseBySubject)
+    #   item = re.sub(r'#\d#', '', str)
+    #   wk_peopleorgs.append(item)
+
+    # print(wk_peopleorgs)
 
     # fix rel-links to be abs-ones
     soup_bodytext = BeautifulSoup(wkbodytext, 'html.parser')
 
-#		for a in soup_bodytext.find_all('a', href=re.compile(r'(\/mediawiki\/.+)')):
-#			rel_link = a.get('href')
-#			print (rel_link)
-			#out_link = urljoin(base_url, rel_link)
-			#a['href'] = out_link
-
     for a in soup_bodytext.find_all('a', href=re.compile(r'^(?!(?:[a-zA-Z][a-zA-Z0-9+.-]*:|//))')):
       rel_link = a.get('href')
-      print(rel_link)
-      print('===')
-      #out_link = urljoin(base_url, rel_link)
-      #print(out_link)
-      #print('***')
-      #a['href'] = out_link
+      rel_link = rel_link.rsplit('/', 1)
+      a['href'] = '/' + rel_link[1]
+
+      # check if `wk_peopleorgs` are present in `wkdata`
+      # if (name in wkbodytext for name in wk_peopleorgs):
+        # if yes, then add `people-orgs/` before the last uri containing peopleorgs
+        # a['href'] = '/peopleorgs/' + rel_link[1]
 
     for img in soup_bodytext.find_all('img', src=re.compile(r'^(?!(?:[a-zA-Z][a-zA-Z0-9+.-]*:|//))')):
       src_rel_link = img.get('src')
@@ -224,10 +241,65 @@ class had(object):
 
     #build template
     return self.render_template('event.html',
-                                #wkdata=data,
                                 nav=wkdata_nav,
                                 title=wktitle,
-                                #date=wkdate,
+                                # date=wkdate,
+                                bodytext=wkbodytext
+                                )
+
+  def on_article(self, request, page_title, section_title, wkdata_nav=nav()):
+    base_url = "http://wikidev.hackersanddesigners.nl/"
+    folder_url = "mediawiki/"
+    api_call =  "api.php?"
+
+    # fetch page-content
+    page_options = {'action': 'parse', 'page': page_title, 'format': 'json', 'formatversion': '2', 'disableeditsection': 'true'}
+    response_content = requests.get(base_url + folder_url + api_call, params=page_options)
+    wkdata = response_content.json()
+    print(response_content.url)
+    
+    wktitle = wkdata['parse']['title']
+    wkbodytext = wkdata['parse']['text']
+
+    wkmeta = wkdata['parse']['links']
+    #wkdate = wkmeta[1]['title']
+
+    # fix rel-links to be abs-ones
+    soup_bodytext = BeautifulSoup(wkbodytext, 'html.parser')
+
+    for a in soup_bodytext.find_all('a', href=re.compile(r'^(?!(?:[a-zA-Z][a-zA-Z0-9+.-]*:|//))')):
+      rel_link = a.get('href')
+      rel_link = rel_link.rsplit('/', 1)
+      a['href'] = '/' + rel_link[1]
+
+    for img in soup_bodytext.find_all('img', src=re.compile(r'^(?!(?:[a-zA-Z][a-zA-Z0-9+.-]*:|//))')):
+      src_rel_link = img.get('src')
+      srcset_rel_link = img.get('srcset')
+      if (src_rel_link):
+        out_link = urljoin(base_url, src_rel_link)
+        img['src'] = out_link
+      if (srcset_rel_link):
+        srcset_list = re.split(r'[,]\s*', srcset_rel_link)
+        srcset_lu = srcset_list
+        srcset_list[:] = [urljoin(base_url, srcset_i) for srcset_i in srcset_list]
+        srcset_s = ', '.join(srcset_lu)
+        img['srcset'] = srcset_s
+
+      # get rid of <a>s wrapping <img>s
+      a_img = img.find_parent("a")
+      a_img.unwrap()
+
+    # delete wiki infobox
+    infobox = soup_bodytext.find('table')
+    infobox.decompose()
+
+    wkbodytext = soup_bodytext
+
+    #build template
+    return self.render_template('article.html',
+                                nav=wkdata_nav,
+                                title=wktitle,
+                                # date=wkdate,
                                 bodytext=wkbodytext
                                 )
 
