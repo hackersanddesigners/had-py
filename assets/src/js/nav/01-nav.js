4f55dfc37a93f961b1df223c7770440944a78d47
afincato <andre.not.me@gmail.com>
2017-05-11 15:22:12 +0200

    add function to show && hide section-menu

--- assets/src/js/nav/01-nav.js
+++ assets/src/js/nav/01-nav.js
@@ -4,18 +4,44 @@
 
 'use strict';
 
-  var waypoint = new Waypoint({
-    element: document.querySelector('.nav'),
-    offset: '-12%',
-    handler: function(direction) {
-      if (direction == 'down') {
-        console.log('cheers 🐟')
-        this.element.classList.add('p-fx', 'p-tl', 'pd-a--2', 'z-2', 'bg-white')
-      } else {
-        console.log('🍄')
-        this.element.classList.remove('p-fx', 'p-tl', 'pd-a--2', 'z-2', 'bg-white')
-      }
-    },
-  })
+var main_menu = document.querySelector('.main-menu')
+var section_menu = document.querySelector('.section-menu')
+var sm = document.createElement('button')
+
+var waypoint = new Waypoint({
+  element: document.querySelector('.nav'),
+  handler: function(direction) {
+    if (direction == 'down') {
+      console.log('cheers 🐟')
+      this.element.classList.add('p-fx', 'p-tl', 'w--full', 'pd-a--2', 'z-2', 'bg-white')
+      section_menu.classList.add('d-n')
+
+      sm.classList.add('sm', 'mg-l--1')
+      main_menu.appendChild(sm)
+    } else {
+      console.log('🍄')
+      this.element.classList.remove('p-fx', 'p-tl', 'w--full', 'pd-a--2', 'z-2', 'bg-white')
+      section_menu.classList.remove('d-n', 'd-ib')
+
+      sm = document.querySelector('.sm')
+      main_menu.removeChild(sm)
+    }
+  },
+  offset: function() {
+    return -this.element.clientHeight
+  }
+})
+
+function section_menu_toggle (event) {
+  if (section_menu.classList.contains('d-n')) {
+    section_menu.classList.remove('d-n')
+    section_menu.classList.add('d-ib')
+  } else {
+    section_menu.classList.remove('d-ib')
+    section_menu.classList.add('d-n')
+  }
+}
+
+sm.addEventListener('click', section_menu_toggle, false)
 
 })(window, document);
