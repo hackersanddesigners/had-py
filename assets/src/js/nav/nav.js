5d8ae402fe8a1e8fdde5ccdd22a58e69e6d5709b
afincato <andre.not.me@gmail.com>
2017-05-12 16:35:14 +0200

    update styles for nav

--- assets/src/js/nav/nav.js
+++ assets/src/js/nav/nav.js
@@ -6,52 +6,68 @@
   
 var nav_wrapper = document.createElement('div')
 var nav = document.querySelector('.nav')
+var mm_wrapper = document.createElement('div')
 var main_menu = document.querySelector('.main-menu')
 var section_menu = document.querySelector('.section-menu')
 var sm = document.createElement('button')
-var boundary = document.querySelector('.boundary')
 
-var boundary = nav.offsetHeight
+var threshold = nav.offsetHeight
+var trigger = threshold / 1.5
 
 window.onscroll = function (event) {
   requestAnimationFrame(checkSticky)
 }
 
 function checkSticky() {
-  var y = window.scrollY + 2
+  var y = window.scrollY - trigger
 
   var isSticky = nav.classList.contains('sticky')
-  if (y > boundary) {
+  if (y > threshold) {
     if (!isSticky) {
       nav.classList.add('sticky')
+      
       // set `nav_wrapper` as `nav`'s parent
-      var parent = nav.parentNode
-      parent.replaceChild(nav_wrapper, nav)
+      var nav_parent = nav.parentNode
+      nav_parent.replaceChild(nav_wrapper, nav)
       nav_wrapper.appendChild(nav)
 
+      var mm_parent = main_menu.parentNode
+      mm_parent.replaceChild(mm_wrapper, main_menu)
+      mm_wrapper.appendChild(main_menu)
+      mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb')
+
+      main_menu.classList.remove('flex-r', 'flex-sb', 'flex-st_s')
+      
       nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-a--2', 'z-2', 'bg-white')
       nav.classList.remove('mg-b--2')
 
       section_menu.classList.add('d-n')
-      sm.classList.add('sm', 'mg-l--1')
-      main_menu.appendChild(sm)
+      sm.classList.add('sm')
+      mm_wrapper.appendChild(sm)
     }
   } else if (isSticky) {
-    nav.classList.remove('sticky')
+    nav.classList.remove('sticky', 'a-eio')
 
-    // remove `nav_wrapper`
-    var doc_frag = document.createDocumentFragment();
-    while (nav_wrapper.firstChild) {
-      var child = nav_wrapper.removeChild(nav_wrapper.firstChild);
-      doc_frag.appendChild(child);
-      nav_wrapper.parentNode.replaceChild(doc_frag, nav_wrapper)
+    sm = document.querySelector('.sm')
+    mm_wrapper.removeChild(sm)
 
-      section_menu.classList.remove('d-n', 'd-ib')
-      sm = document.querySelector('.sm')
-      main_menu.removeChild(sm)
-    }
+    unwrap(nav_wrapper)
+    unwrap(mm_wrapper)
+
+    section_menu.classList.remove('d-n', 'd-ib')
   }
 }
+
+function unwrap(wrapper) {
+  var doc_frag = document.createDocumentFragment()
+  while (wrapper.firstChild) {
+    var child = wrapper.removeChild(wrapper.firstChild)
+    doc_frag.appendChild(child)
+  }
+      
+  wrapper.parentNode.replaceChild(doc_frag, wrapper)
+}
+
 
 function section_menu_toggle (event) {
   if (section_menu.classList.contains('d-n')) {
