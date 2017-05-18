4773ff75c327ce29e668de80cf7a923d8e31491b
afincato <andre.not.me@gmail.com>
2017-05-18 12:33:45 +0200

    navbar now works as I want to but console keeps throwing errors

--- assets/src/js/nav/nav.js
+++ assets/src/js/nav/nav.js
@@ -8,52 +8,86 @@ var mm_wrapper = document.createElement('div')
 var main_menu = document.querySelector('.main-menu')
 var section_menu = document.querySelector('.section-menu')
 var sm = document.createElement('button')
-
 var intro = document.querySelector('.intro')
 
-nav.classList.remove('mg-b--2')
-section_menu.classList.add('d-n')
+function onResizing (event) {
+  
+  // --- make toggle menu
+  if (window.innerWidth < 640) {
+    // clean up space between `nav` and `intro p`
+    nav.classList.remove('mg-b--2')
+    section_menu.classList.add('d-n')
 
-intro.classList.remove('mg-t--3')
-intro.classList.add('mg-t--2')
+    intro.classList.remove('mg-t--3')
+    intro.classList.add('mg-t--2')
 
-var threshold = nav.offsetHeight
-var trigger = threshold / 2
+    // set `nav_wrapper` as `nav`'s parent
+    var nav_parent = nav.parentNode
+    nav_parent.replaceChild(nav_wrapper, nav)
+    nav_wrapper.appendChild(nav)
 
-window.onscroll = function (event) {
-  requestAnimationFrame(checkSticky)
-}
+    var mm_parent = main_menu.parentNode
+    mm_parent.replaceChild(mm_wrapper, main_menu)
+    mm_wrapper.appendChild(main_menu)
+    mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb', 'flex-ab')
 
-function checkSticky() {
-  var y = window.scrollY - trigger
+    nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white')
+    nav.classList.remove('mg-b--2')
 
-  var isSticky = nav.classList.contains('sticky')
-  if (y > threshold) {
-    if (!isSticky) {
-      nav.classList.add('sticky')
-      
-      // set `nav_wrapper` as `nav`'s parent
-      var nav_parent = nav.parentNode
-      nav_parent.replaceChild(nav_wrapper, nav)
-      nav_wrapper.appendChild(nav)
+    // wrap section menu to toggle button
+    section_menu.classList.add('d-n')
+    sm.classList.add('ft-2', 'ft-3_m', 'blue')
+    sm.innerHTML = '•••'
+    mm_wrapper.appendChild(sm)
+  } else {
+    // --- set scroll trigger
+    var threshold = nav.offsetHeight
+    var trigger = threshold / 2
 
-      var mm_parent = main_menu.parentNode
-      mm_parent.replaceChild(mm_wrapper, main_menu)
-      mm_wrapper.appendChild(main_menu)
-      mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb', 'flex-ab')
-      
-      nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white')
-      nav.classList.remove('mg-b--2')
-
-      section_menu.classList.add('d-n')
-      sm.classList.add('sm', 'ft-2', 'ft-3_m', 'blue')
-      sm.innerHTML = '•••'
-      mm_wrapper.appendChild(sm)
+    window.onscroll = function (event) {
+      requestAnimationFrame(checkSticky)
     }
-  } else if (isSticky) {
-    nav.classList.remove('sticky')
+    
+    // --- set function to check for scroll trigger
+    function checkSticky() {
+      var y = window.scrollY - trigger
 
-    sm = document.querySelector('.sm')
+      var isSticky = nav.classList.contains('sticky')
+      if (y > threshold) {
+        if (!isSticky) {
+          nav.classList.add('sticky')
+        
+          // set `nav_wrapper` as `nav`'s parent
+          var nav_parent = nav.parentNode
+          nav_parent.replaceChild(nav_wrapper, nav)
+          nav_wrapper.appendChild(nav)
+
+          var mm_parent = main_menu.parentNode
+          mm_parent.replaceChild(mm_wrapper, main_menu)
+          mm_wrapper.appendChild(main_menu)
+          mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb', 'flex-ab')
+      
+          nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white')
+          nav.classList.remove('mg-b--2')
+        
+          section_menu.classList.add('d-n')
+          sm.classList.add('ft-2', 'ft-3_m', 'blue')
+          sm.innerHTML = '•••'
+          mm_wrapper.appendChild(sm)
+        }
+      } else if (isSticky) {
+        nav.classList.remove('sticky')
+
+        // unwrap section menu
+        mm_wrapper.removeChild(sm)
+
+        unwrap(nav_wrapper)
+        unwrap(mm_wrapper)
+
+        section_menu.classList.remove('d-n', 'd-ib')
+      }
+    }
+    // unwrap section menu
     mm_wrapper.removeChild(sm)
 
     unwrap(nav_wrapper)
@@ -61,16 +95,17 @@ function checkSticky() {
 
     section_menu.classList.remove('d-n', 'd-ib')
   }
-}
 
-function unwrap(wrapper) {
-  var doc_frag = document.createDocumentFragment()
-  while (wrapper.firstChild) {
-    var child = wrapper.removeChild(wrapper.firstChild)
-    doc_frag.appendChild(child)
-  }
+  function unwrap(wrapper) {
+    var doc_frag = document.createDocumentFragment()
+    while (wrapper.firstChild) {
+      var child = wrapper.removeChild(wrapper.firstChild)
+      doc_frag.appendChild(child)
+    }
       
-  wrapper.parentNode.replaceChild(doc_frag, wrapper)
+    wrapper.parentNode.replaceChild(doc_frag, wrapper)
+  }
+
 }
 
 function section_menu_toggle (event) {
@@ -82,6 +117,11 @@ function section_menu_toggle (event) {
     section_menu.classList.add('d-n')
   }
 }
+
+// - - - - -
+
+window.onresize = onResizing;
+window.onload = onResizing;
 
 sm.addEventListener('click', section_menu_toggle, false)
 
