1235af67a399a2ea9626790e8bb868005ebca2a9
afincato <andre.not.me@gmail.com>
2017-05-21 16:11:34 +0200

    re-simplified navbar

--- assets/src/js/nav/nav.js
+++ assets/src/js/nav/nav.js
@@ -5,9 +5,7 @@
 var nav_wrapper = document.createElement('div');
 var nav = document.querySelector('.nav');
 var nav_parent = nav.parentNode;
-var mm_wrapper = document.createElement('div');
 var main_menu = document.querySelector('.main-menu');
-var mm_parent = main_menu.parentNode;
 var section_menu = document.querySelector('.section-menu');
 var sm = document.createElement('button');
 var sm_i = document.querySelectorAll('.sm-item');
@@ -15,100 +13,51 @@ var intro = document.querySelector('.intro');
 
 function onResizing (event) {
   
-  // --- make toggle menu
-  if (window.innerWidth > 640) {
-    
-    // --- set scroll trigger
-    var threshold = nav.offsetHeight;
-    var trigger = threshold / 2;
+  // --- set scroll trigger
+  var threshold = nav.offsetHeight;
+  var trigger = threshold / 2;
 
-    window.onscroll = function (event) {
-      requestAnimationFrame(checkSticky);
-    };
+  window.onscroll = function (event) {
+    requestAnimationFrame(checkSticky);
+  };
     
-    // --- set function to check for scroll trigger
-    function checkSticky() {
-      var y = window.scrollY - trigger;
+  // --- set function to check for scroll trigger
+  function checkSticky() {
+    var y = window.scrollY - trigger;
+    
+    if (y > threshold) {
+      if (!nav.classList.contains('sticky')) {
+        
+      // set up `nav`
+      nav.classList.add('sticky', 'd-n');
+
+      // set `nav_wrapper` as `nav`'s parent
+      nav_parent.replaceChild(nav_wrapper, nav);
+      nav_wrapper.appendChild(nav);
+      nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'z-2');
+      nav.classList.remove('mg-b--2');
       
-      if (y > threshold) {
+      // set up `sm`
+      sm.classList.add('z-3', 'p-abs', 'p-tr', 'ft-2', 'ft-3__m', 'blue');
+      sm.innerHTML = '•••';
+      nav_wrapper.insertBefore(sm, nav);
+    }
+  } else if (nav.classList.contains('sticky')) {
+    nav.classList.remove('sticky', 'd-n', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
         
-        if (!nav.classList.contains('sticky') && nav.parentNode !== nav_wrapper) {
-          nav.classList.add('sticky');
-
-          // set `nav_wrapper` as `nav`'s parent
-          nav_parent.replaceChild(nav_wrapper, nav);
-          nav_wrapper.appendChild(nav);
-
-          mm_parent.replaceChild(mm_wrapper, main_menu);
-          mm_wrapper.appendChild(main_menu);
-          mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb', 'flex-ab');
-    
-          nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
-          nav.classList.remove('mg-b--2');
-    
-          section_menu.classList.add('d-n');
-          sm.classList.add('ft-2', 'ft-3__m', 'blue');
-          sm.innerHTML = '•••';
-          mm_wrapper.appendChild(sm);
-        }
-
-      } else if (y < threshold && nav.classList.contains('sticky')) {
-        nav.classList.remove('sticky');
-        
-        for(var i = 0; i < sm_i.length; i++) {
-          sm_i[i].classList.remove('w--half', 'w--one-third__m', 'w--one-fourth__l', 'mg-b--1');
-          sm_i[i].classList.add('mg-r--1', 'mg-b--05');
-        }
-
-        // unwrap section menu
-        mm_wrapper.removeChild(sm);
-        unwrap(nav_wrapper);
-        unwrap(mm_wrapper);
-
-        section_menu.classList.remove('d-n', 'd-ib', 'flex-r', 'flex-ab', 'flex-sb');
-      }
-    } // --- end of checkSticky()
-
-    // unwrap section menu b/t shifting point (640px)
-    // this works okay if `y < threshold`, but does not care if `y > threshold` && window is being resized from < 640 to > 640
-    // but it works okay if window is being resized from > 640 to < 640 (w/ `threshold > y`)
-    if (!nav.classList.contains('sticky') && section_menu.classList.contains('d-n')) {
-      // unwrap section menu
-      mm_wrapper.removeChild(sm);
-
-      unwrap(nav_wrapper);
-      unwrap(mm_wrapper);
-
-      section_menu.classList.remove('d-n', 'd-ib');
+    for(var i = 0; i < sm_i.length; i++) {
+      sm_i[i].classList.remove('w--half', 'w--one-third__m', 'w--one-fourth__l', 'mg-b--1');
+      sm_i[i].classList.add('mg-r--1', 'mg-b--05');
     }
 
-  // --- * * *
+    // unwrap section menu
+    nav_wrapper.removeChild(sm);
+    unwrap(nav_wrapper);
 
-  } else if (window.innerWidth < 640) {
-      if (nav.parentNode !== nav_wrapper) {
-        // clean up space between `nav` and `intro p`
-        nav.classList.remove('mg-b--2');
-    
-        // set `nav_wrapper` as `nav`'s parent
-        nav_parent.replaceChild(nav_wrapper, nav);
-        nav_wrapper.appendChild(nav);
-
-        mm_parent.replaceChild(mm_wrapper, main_menu);
-        mm_wrapper.appendChild(main_menu);
-        mm_wrapper.classList.add('mm_wrapper', 'flex-r', 'flex-sb', 'flex-ab');
-
-        nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
-
-        // wrap section menu to toggle button
-        section_menu.classList.add('d-n');
-        sm.classList.add('ft-2', 'ft-3__m', 'blue');
-        sm.innerHTML = '•••';
-        mm_wrapper.appendChild(sm);
-      } else if (nav.classList.contains('sticky')) {
-        nav.classList.remove('sticky');
-    }
-  }
-}
+    section_menu.classList.remove('d-ib', 'flex-r', 'flex-ab', 'flex-sb');
+   }
+  } // --- end of checkSticky()
+} // --- end resize()
 
 function unwrap(wrapper) {
   var doc_frag = document.createDocumentFragment();
@@ -120,21 +69,25 @@ function unwrap(wrapper) {
 }
 
 function section_menu_toggle (event) {
-  if (section_menu.classList.contains('d-n')) {
-    section_menu.classList.remove('d-n');
+  if (nav.classList.contains('d-n')) {
+    nav.classList.remove('d-n');
+    nav.classList.add('pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
+    nav_wrapper.classList.add('pd-b--1');
     section_menu.classList.add('flex-r', 'flex-ab', 'flex-sb');
+    
     for(var i = 0; i < sm_i.length; i++) {
       sm_i[i].classList.remove('mg-r--1', 'mg-b--05');
       sm_i[i].classList.add('w--half', 'w--one-third__m', 'w--one-fourth__l', 'mg-b--1');
     }
-    nav_wrapper.classList.add('pd-b--1');
   } else {
+    nav.classList.add('d-n');
+    nav.classList.remove('pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
+    nav_wrapper.classList.remove('pd-b--1');
     section_menu.classList.remove('d-ib');
-    section_menu.classList.add('d-n');
+    
     for(var i = 0; i < sm_i.length; i++) {
       sm_i[i].classList.remove('w--half', 'w--one-third__m', 'w--one-fourth__l', 'mg-b--1');
     }
-    nav_wrapper.classList.remove('pd-b--1');
   }
 }
 
