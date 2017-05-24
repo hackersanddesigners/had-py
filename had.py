af95b28a940baa5fc9f8aef3a6d9f7ea02d75840
afincato <andre.not.me@gmail.com>
2017-05-24 13:45:22 +0200

    trying again to sort with lambda

--- had.py
+++ had.py
@@ -7,7 +7,6 @@ from werkzeug.utils import redirect
 import requests
 import datetime
 from dateutil.parser import parse
-from operator import itemgetter, attrgetter, methodcaller
 import re
 from bs4 import BeautifulSoup, Comment
 from urllib.parse import urljoin, quote
@@ -231,7 +230,7 @@ class had(object):
         wkdata_upevents.append(p_intro)
 
     wkdata_upevents = list(zip(*[iter(wkdata_upevents)]*3))
-    # sorted(wkdata_upevents, key=itemgetter(1))
+    wkdata_upevents = sorted(wkdata_upevents, key=lambda x: x[1])
 
     # --- past events
     wkdata_pastevents = []
@@ -243,7 +242,7 @@ class had(object):
         wkdata_pastevents.append(date)
     
     wkdata_pastevents = list(zip(*[iter(wkdata_pastevents)]*2))
-    # sorted(wkdata_pastevents, key=itemgetter(1), reverse=True)
+    wkdata_pastevents = sorted(wkdata_pastevents, key=lambda x: x[1], reverse=True)
 
     # build template
     return self.render_template('event_list.html',
