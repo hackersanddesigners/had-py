21d17cdc1c769253a9356d36c3af6c2269d0c3e0
afincato <andre.not.me@gmail.com>
2017-07-06 17:44:55 +0200

    nav: add rule to check if dh > wh

--- assets/src/js/nav/nav.js
+++ assets/src/js/nav/nav.js
@@ -10,30 +10,40 @@ var main_menu = document.querySelector('.main-menu');
 var mm_parent = main_menu.parentNode;
 var sm = document.querySelector('.section-menu');
 var toggle = document.createElement('button');
+var main = document.querySelector('main');
 
 function on_resizing (event) {
 
   var ww = window.innerWidth;
+  var wh = window.innerHeight;
+  var dh = document.body.scrollHeight
+  
+  var n_wh = main.previousElementSibling.getBoundingClientRect();
+  var nh = n_wh.height;
+  console.log('*** nh ' + nh);
 
-  sticky();
+  // check if document height 
+  // is bigger than window height
+  console.log(dh + ' > ' + wh);
+  if (dh > wh) {
+    sticky();
+  }
 
-  // unwrap sb if > 640 and !sticky
+  // unwrap sm if > 640 and !sticky
   if (ww > 640 && !nav.classList.contains('sticky')) {
-
     if (document.body.contains(toggle)) {
-      unwrap_sb();
+      unwrap_sm();
     }
 
   } else {
     if (!document.body.contains(mm_wrapper)) {
-      wrap_sb();
+      wrap_sm(nh);
     }
-
   }
-  
+
 }
 
-function wrap_sb () {
+function wrap_sm (nh) {
   // hide `sm`
   sm.classList.add('d-n');
 
@@ -47,9 +57,14 @@ function wrap_sb () {
   
   nav_wrapper.classList.add('p-fx', 'p-tl', 'w--full', 'z-2');
 
-  nav_wrapper.nextElementSibling.classList.remove('mg-t--3');
-  var nav_wh = nav_wrapper.offsetHeight;
-  nav_wrapper.nextElementSibling.style.paddingTop = nav_wh + 'px';
+  // smooth in nav transition when scrolling
+  var nw_wh = main.previousElementSibling.getBoundingClientRect();
+  var nwh = nw_wh.height;
+
+  console.log('±± nh ' + nh);
+
+  main.classList.remove('mg-t--3');
+  main.style.paddingTop = nwh + 'px';
 
   nav.classList.add('pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
   nav.classList.remove('mg-b--2');
@@ -60,13 +75,16 @@ function wrap_sb () {
   nav_wrapper.insertBefore(toggle, nav);
 }
 
-function unwrap_sb() {
+function unwrap_sm() {
   nav_wrapper.removeChild(toggle);
   unwrap(nav_wrapper);
   unwrap(mm_wrapper);
 
   nav.classList.remove('pd-t--1', 'pd-h--2', 'z-2', 'bg-white');
   sm.classList.remove('d-n');
+
+  main.classList.add('mg-t--3');
+  main.removeAttribute('style');
 }
 
 function sticky () {
@@ -83,18 +101,18 @@ function sticky () {
     var y = window.scrollY - trigger;
 
     // toggle sticky class &&
-    // wrap/unwrap sb if ww > 640
+    // wrap/unwrap sm if ww > 640
     if (y > threshold) {
       nav.classList.add('sticky');
       
       if (window.innerWidth > 640 && !document.body.contains(mm_wrapper)) {
-        wrap_sb();
+        wrap_sm();
       }
     } else {
       nav.classList.remove('sticky');
 
       if (window.innerWidth > 640 && document.body.contains(toggle)) {
-        unwrap_sb();
+        unwrap_sm();
       }
     }
 
@@ -119,6 +137,7 @@ function sm_toggle (event) {
     sm.classList.remove('pd-b--2', 'pd-b--1__m');
   }
 }
+
 
 // - - - - -
 
